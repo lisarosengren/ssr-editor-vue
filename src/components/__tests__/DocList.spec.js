2fd47e084edde31557fe8c73715267444400cb3f
Emma <emmawidengrd@yahoo.se>
2025-09-27 12:48:22 +0200

    small updates, add test for onClick

--- src/components/__tests__/DocList.spec.js
+++ src/components/__tests__/DocList.spec.js
@@ -1,5 +1,5 @@
 import { test, expect, vi } from 'vitest';
-import { mount, flushPromises } from '@vue/test-utils';
+import { mount, flushPromises, RouterLinkStub } from '@vue/test-utils';
 import DocList from '../DocList.vue';
 import { getAll } from '@/models/docs';
 
@@ -8,26 +8,49 @@ vi.mock('@/models/docs', () => ({
   getAll: vi.fn()
 }));
 
-test('mock docs', async () => {
-  // const mockRouter = { push: vi.fn() };
+const mockPush = vi.fn();
+
+test('that component renders list of doc links', async () => {
   getAll.mockResolvedValueOnce([
     { _id: '1234', title: 'Test doc 1'},
     { _id: '5678', title: 'Test doc 2'}
   ]);
 
   const wrapper = mount(DocList, {
-    // global: {
-    //   mocks: {
-    //     $router: mockRouter
-    //   },
-    //   stubs: {
-    //     RouterLink: true
-    //   }
-    // }
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    }
   });
   await flushPromises();
 
   const mockDocs = wrapper.findAll('li');
   expect(mockDocs).toHaveLength(2);
   expect(mockDocs[0].text()).toContain('Test doc 1');
+});
+
+test('that onClick is called when link is clicked', async () => {
+    getAll.mockResolvedValueOnce([
+    { _id: '1234', title: 'Test doc 1'},
+  ]);
+
+  const consoleSpy = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+  const wrapper = mount(DocList, {
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    }
+  });
+
+  await flushPromises();
+
+  const mockLink = wrapper.findComponent(RouterLinkStub);
+  await mockLink.trigger('click');
+
+  expect(consoleSpy).toHaveBeenCalledWith('Clicked');
+
+  consoleSpy.mockRestore();
 });

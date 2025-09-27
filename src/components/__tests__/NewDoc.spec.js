b4100aa92250c213de4882bdadc6d055b0d1210c
Emma <emmawidengrd@yahoo.se>
2025-09-27 12:45:37 +0200

    add test for insert fail

--- src/components/__tests__/NewDoc.spec.js
+++ src/components/__tests__/NewDoc.spec.js
@@ -8,11 +8,48 @@ vi.mock('@/models/docs', () => ({
   newDoc: vi.fn()
 }));
 
+const mockPush = vi.fn();
 
-test('mock doc insertion', async () => {
+test('mock successful doc insertion', async () => {
   newDoc.mockResolvedValueOnce('mock doc id');
 
-  const wrapper = mount(NewDoc)
-  expect(wrapper.exists()).toBe(true)
+  const wrapper = mount(NewDoc, {
+    global: {
+      mocks: {
+        $router: {
+          push: mockPush
+        }
+      }
+    }
+  });
+  await wrapper.find('input[type="text"').setValue('mock insert title');
+  await wrapper.find('textarea').setValue('mock insert content');
+  await wrapper.find('form').trigger('submit');
   await flushPromises();
+  expect(newDoc).toHaveBeenCalledWith({
+    title: 'mock insert title',
+    content: 'mock insert content'
+  })
+
+  expect(mockPush).toHaveBeenCalledWith('/mock doc id');
+});
+
+test('mock doc insertion failure', async () => {
+  newDoc.mockRejectedValueOnce(new Error('Database error'));
+
+  const wrapper = mount(NewDoc, {
+    global: {
+      mocks: {
+        $router: {
+          push: mockPush
+        }
+      }
+    }
+  });
+  await wrapper.find('input[type="text"').setValue('mock insert title');
+  await wrapper.find('textarea').setValue('mock insert content');
+  await wrapper.find('form').trigger('submit');
+  await flushPromises();
+  expect(wrapper.find('.err').exists()).toBe(true);
+  expect(newDoc).toHaveBeenCalled();
 });

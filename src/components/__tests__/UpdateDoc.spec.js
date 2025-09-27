640f02e320ee1527a905a3f8ca0607385e528a9b
Emma <emmawidengrd@yahoo.se>
2025-09-27 12:58:22 +0200

    fix some flaws

--- src/components/__tests__/UpdateDoc.spec.js
+++ src/components/__tests__/UpdateDoc.spec.js
@@ -1,16 +1,29 @@
 import { test, expect, vi } from 'vitest';
 import { flushPromises, mount } from '@vue/test-utils';
 import UpdateDoc from '../UpdateDoc.vue';
-import { updateDoc } from '@/models/docs';
+import { updateDoc, getOne } from '@/models/docs';
+import { useRoute } from 'vue-router';
 
-// mocking user input
+// mock useRoute, getOne and updateDoc
 vi.mock('@/models/docs', () => ({
+  getOne: vi.fn(),
   updateDoc: vi.fn()
 }));
 
+vi.mock('vue-router', () => ({
+  useRoute: vi.fn()
+}));
 
 test('mock doc insertion', async () => {
-  updateDoc.mockResolvedValueOnce('mock doc id');
+  useRoute.mockReturnValue({
+    params: { id: 'mock id'}
+  });
+  getOne.mockResolvedValueOnce({
+    _id: 'mock id',
+    title: 'mock title before',
+    content: 'mock content before'
+  });
+  updateDoc.mockResolvedValueOnce();
 
   const wrapper = mount(UpdateDoc)
   expect(wrapper.exists()).toBe(true)

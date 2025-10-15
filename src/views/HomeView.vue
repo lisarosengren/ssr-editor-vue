277bdcf96bc73d2bc593209d6bcb2681b05e005a
Emma <emmawidengrd@yahoo.se>
2025-10-15 11:46:32 +0200

    Loads UserDocs if logged in, else loads UserLogin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup>
-import DocList from '@/components/DocList.vue';
+import { ref, onMounted } from 'vue';
+import UserDocs from '@/components/UserDocs.vue';
+import UserLogin from '@/components/UserLogin.vue';
+import { getUser } from '@/models/docs';
+
+const loggedIn = ref(false);
+const user = ref(null);
+
+onMounted(async () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const currentUser = await getUser();
+    if (currentUser) {
+      user.value = currentUser;
+      loggedIn.value = true;
+      console.log("logged in")
+    }
+  }
+});
+
+function handleLogin(loggedInUser) {
+  user.value = loggedInUser;
+  loggedIn.value = true;
+}
 </script>
 
 <template>
   <main>
-    <DocList />
+    <UserDocs v-if="loggedIn" :user="user" />
+    <UserLogin v-else @login-success="handleLogin" />
   </main>
 </template>
